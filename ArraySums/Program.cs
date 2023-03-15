49488a5fe0176b2c020da9749cb535dd235d69e5
TomasCarvalho <2606tom@gmail.com>
2023-03-15 17:20:08 +0000

    Finish implementing matrix entry

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -6,7 +6,22 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter nº of lines:");
+            int lines=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter nº of columns:");
+            int columns=int.Parse(Console.ReadLine());
+
+            float[,] matrix= new float [lines,columns];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Enter value for position [{i+1},{j+1}]:");
+                    matrix[i, j] = float.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine(matrix);
         }
     }
 }
