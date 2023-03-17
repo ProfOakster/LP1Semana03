2316e5e5bb497b730b997deb6d209a67b43ec062
TomasCarvalho <2606tom@gmail.com>
2023-03-17 11:22:46 +0000

    Implement PlayerPowers

---
+++ PlayerPowers/Powers.cs
@@ -0,0 +1,10 @@
+using System;
+[Flags]
+enum Powers
+{
+    None = 0,
+    Fly = 1 << 0,
+    XRayVision = 1 << 1,
+    SuperStrength = 1 << 2
+};
+
---
+++ PlayerPowers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace PlayerPowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input nº of players:");
+            int n = int.Parse(Console.ReadLine());
+
+            Powers [] CharsPower = new Powers[n];
+
+            string input;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Select player {i+1}'s powers: [Y/N]");
+                for (int p = 0; p < 3; p++)
+                {
+                    Console.WriteLine((Powers)(1 << p));
+                    input = Console.ReadLine();
+                    if (input.ToLower()=="y")
+                    {
+                        CharsPower[i] ^= (Powers)(1 << p);
+                    };
+                }
+            }
+
+            for (int c = 0; c < n; c++)
+            {
+                Console.WriteLine($"Player {c+1}'s powers:");
+                Console.WriteLine(CharsPower[c]);
+                if ((CharsPower[c] & (Powers)5)==(Powers)5)
+                {
+                   Console.WriteLine("Flying radiation!"); 
+                }
+
+            }
+        }
+    }
+}
